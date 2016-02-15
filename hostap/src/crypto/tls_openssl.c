fda39b67c73e3c4651aba565f3aa23de2291ea36
Jouni Malinen <jouni@qca.qualcomm.com>
2016-02-16 00:34:42 +0200

    OpenSSL: Fix memory leak in subjectAltName parsing
    The parsed data from X509_get_ext_d2i() needs to be freed.
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>

--- hostap/src/crypto/tls_openssl.c
+++ hostap/src/crypto/tls_openssl.c
@@ -1425,6 +1425,8 @@ static int tls_match_altsubject_component(X509 *cert, int type,
 			found++;
 	}
 
+	sk_GENERAL_NAME_pop_free(ext, GENERAL_NAME_free);
+
 	return found;
 }
 
@@ -1537,9 +1539,11 @@ static int tls_match_suffix(X509 *cert, const char *match, int full)
 		    1) {
 			wpa_printf(MSG_DEBUG, "TLS: %s in dNSName found",
 				   full ? "Match" : "Suffix match");
+			sk_GENERAL_NAME_pop_free(ext, GENERAL_NAME_free);
 			return 1;
 		}
 	}
+	sk_GENERAL_NAME_pop_free(ext, GENERAL_NAME_free);
 
 	if (dns_name) {
 		wpa_printf(MSG_DEBUG, "TLS: None of the dNSName(s) matched");
@@ -1736,6 +1740,7 @@ static void openssl_tls_cert_event(struct tls_connection *conn,
 		pos += gen->d.ia5->length;
 		*pos = '\0';
 	}
+	sk_GENERAL_NAME_pop_free(ext, GENERAL_NAME_free);
 
 	for (alt = 0; alt < num_altsubject; alt++)
 		ev.peer_cert.altsubject[alt] = altsubject[alt];
